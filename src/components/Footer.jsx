8a878283e4d0dedb9756ca95d03de451bc0a6e7a
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-10-31 00:36:05 +1100

    style: add size prop to ArrowUp icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
         <p className="text-sm text-muted-foreground"> &copy; {new Date().getFullYear()} Saitech.co. All rights reserved. </p>
         <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transtion-colors">
-            <ArrowUp />
+            <ArrowUp size={20}/>
 
         </a>
     </footer>
